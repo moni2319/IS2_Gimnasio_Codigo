060622cb833424d3f615512b6c85e8fb470d00d4
TenshiZ <114061087+TenshiZ@users.noreply.github.com>
2023-05-01 23:17:10 +0200

    FactoriaDAO

---
+++ Codigo_GimnasioAdmin/src/integracion/factoria/FactoriaDAO.java
@@ -0,0 +1,14 @@
+package integracion.factoria;
+
+import integracion.actividad.DAOActividad;
+
+
+public interface FactoriaDAO {
+	//descomentar mientras se va haciendo
+	public DAOActividad getDAOActividad();
+	// public DAOEntrenamiento getDAOEntrenamiento();
+	// public DAOCliente getDAOACliente();
+	// public DAOFactura getDAOFactura();
+	// public DAOMaterial getDAOServicio();
+	// public DAOMonitor getDAOTren();
+}
