e5c39ca0b368e836bdf30c62cdd3b12aafba73b8
TenshiZ <114061087+TenshiZ@users.noreply.github.com>
2023-05-01 15:11:42 +0200

    presentacion creado y movido las clases

--- Codigo_GimnasioAdmin/src/presentacion/ClientWindow.java
+++ Codigo_GimnasioAdmin/src/presentacion/ClientWindow.java
@@ -1,3 +1,4 @@
+package presentacion;
 
 import javax.swing.*;
 import java.awt.*;
--- Codigo_GimnasioAdmin/src/presentacion/MainWindow.java
+++ Codigo_GimnasioAdmin/src/presentacion/MainWindow.java
@@ -1,5 +1,7 @@
+package presentacion;
 
 import javax.swing.*;
+
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
