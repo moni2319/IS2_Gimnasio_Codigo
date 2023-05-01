441f68cb3deed0549a55dfe07abff013d513ca9a
TenshiZ <114061087+TenshiZ@users.noreply.github.com>
2023-05-01 15:19:22 +0200

    MonitorWindow general

--- Codigo_GimnasioAdmin/src/presentacion/MainWindow.java
+++ Codigo_GimnasioAdmin/src/presentacion/MainWindow.java
@@ -16,11 +16,11 @@ public class MainWindow extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setTitle("Gimnasio");
 
-		// Panel principal
+		//panel
 		JPanel panel = new JPanel(new GridLayout(3, 1, 10, 10));
 		panel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
 
-		// Botones principales
+		//botones principales
 		JButton clienteButton = new JButton("Clientes");
 		JButton monitorButton = new JButton("Monitores");
 		JButton entrenamientoButton = new JButton("Entrenamientos");
@@ -28,17 +28,17 @@ public class MainWindow extends JFrame {
 		JButton materialButton = new JButton("Materiales");
 		JButton facturaButton = new JButton("Facturas");
 
-		// Agregar botones principales al panel
+		//añadir botones
 		panel.add(clienteButton);
 		panel.add(monitorButton);
 		panel.add(entrenamientoButton);
 		panel.add(actividadButton);
 		panel.add(materialButton);
 		panel.add(facturaButton);
-
+		//añadir el panel
 		getContentPane().add(panel);
 
-		// Configurar acciones de los botones principales
+		//Acciones de los botones
 		clienteButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				// Abrir la ventana de clientes
@@ -57,6 +57,7 @@ public class MainWindow extends JFrame {
 
 		entrenamientoButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				// Abrir la ventana de entrenamiento
 
 			}
 		});
@@ -71,11 +72,13 @@ public class MainWindow extends JFrame {
 
 		materialButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				// Abrir la ventana de materiales
 
 			}
 		});
 		facturaButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				// Abrir la ventana de facturas
 
 			}
 		});
