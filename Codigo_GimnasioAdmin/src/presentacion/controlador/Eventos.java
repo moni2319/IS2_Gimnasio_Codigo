949a9fa54985948a5a706a7d030bb9d3622713ff
TenshiZ <114061087+TenshiZ@users.noreply.github.com>
2023-05-02 22:16:14 +0200

    MonitorEventos

--- Codigo_GimnasioAdmin/src/presentacion/controlador/Eventos.java
+++ Codigo_GimnasioAdmin/src/presentacion/controlador/Eventos.java
@@ -23,6 +23,15 @@ public class Eventos {
 	public static final int MOSTRAR_LISTA_MATERIAL = 115;
 	public static final int MODIFICAR_MATERIAL = 116;
 
+	// MONITOR
+	public static final int ALTA_MONITOR = 117;
+	public static final int BAJA_MONITOR = 118;
+	public static final int MOSTRAR_MONITOR = 119;
+	public static final int MOSTRAR_LISTA_MONITOR = 120;
+	public static final int MODIFICAR_MONITOR = 121;
+
+	// _________________________________________________________________________________________________________
+
 	// ACTIVIDAD
 	public static final int ALTA_ACTIVIDAD_BIEN = 501;
 	public static final int BAJA_ACTIVIDAD_BIEN = 502;
@@ -44,5 +53,12 @@ public class Eventos {
 	public static final int MOSTRAR_MATERIAL_BIEN = 514;
 	public static final int MOSTRAR_LISTA_MATERIAL_BIEN = 515;
 	public static final int MODIFICAR_MATERIAL_BIEN = 516;
+
+	// MONITOR
+	public static final int ALTA_MONITOR_BIEN = 517;
+	public static final int BAJA_MONITOR_BIEN = 518;
+	public static final int MOSTRAR_MONITOR_BIEN = 519;
+	public static final int MOSTRAR_LISTA_MONITOR_BIEN = 520;
+	public static final int MODIFICAR_MONITOR_BIEN = 521;
 
 }
