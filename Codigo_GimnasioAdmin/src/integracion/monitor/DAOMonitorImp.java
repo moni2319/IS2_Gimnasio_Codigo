6db35ed9fed20d44b5f2061fcc5709c8d52f1a3b
TenshiZ <114061087+TenshiZ@users.noreply.github.com>
2023-05-02 22:04:58 +0200

    DAOMonitoImp Entero

--- Codigo_GimnasioAdmin/src/integracion/monitor/DAOMonitorImp.java
+++ Codigo_GimnasioAdmin/src/integracion/monitor/DAOMonitorImp.java
@@ -2,10 +2,11 @@ package integracion.monitor;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
-
+import negocio.material.TransMaterial;
 import negocio.monitor.TransMonitor;
 
 public class DAOMonitorImp implements DAOMonitor {
@@ -22,7 +23,7 @@ public class DAOMonitorImp implements DAOMonitor {
 			daoMonitor = new DAOMonitorImp(connection);
 		return daoMonitor;
 	}
-	
+
 	public boolean altaMonitor(TransMonitor tMonitor) {
 		String query = "INSERT INTO monitor (id, nombre, salario) VALUES (?, ?, ?)";
 		try (PreparedStatement st = connection.prepareStatement(query)) {
@@ -39,26 +40,81 @@ public class DAOMonitorImp implements DAOMonitor {
 	}
 
 	public boolean bajaMonitor(int id) {
-		// TODO Auto-generated method stub
-		return false;
+		String query = "DELETE FROM monitor WHERE id = ?";
+		try (PreparedStatement st = connection.prepareStatement(query)) {
+			st.setInt(1, id);
+			int rowsAffected = st.executeUpdate();
+			return rowsAffected > 0;
+		} catch (SQLException e) {
+			System.err.print(e.getMessage());
+			e.printStackTrace();
+			return false;
+		}
 	}
 
-	@Override
 	public boolean modificarMonitor(TransMonitor tMonitor) {
-		// TODO Auto-generated method stub
-		return false;
+		String query = "UPDATE monitor SET nombre = ?, stock = ? WHERE id = ?";
+		try (PreparedStatement st = connection.prepareStatement(query)) {
+			st.setString(1, tMonitor.getNombre());
+			st.setInt(2, tMonitor.getSalario());
+			st.setInt(3, tMonitor.getId());
+			int rowsAffected = st.executeUpdate();
+			return rowsAffected > 0;
+		} catch (SQLException e) {
+			System.err.print(e.getMessage());
+			e.printStackTrace();
+			return false;
+		}
 	}
 
-	@Override
 	public TransMonitor buscarMonitor(int id) {
-		// TODO Auto-generated method stub
-		return null;
+		String query = "SELECT * FROM material WHERE id = ?";
+		try (PreparedStatement st = connection.prepareStatement(query)) {
+			st.setInt(1, id);
+			ResultSet rs = st.executeQuery();
+			return getNextMonitor(rs);
+		} catch (SQLException e) {
+			System.err.print(e.getMessage());
+			e.printStackTrace();
+			return null;
+		}
 	}
 
-	@Override
+	public TransMonitor getNextMonitor(ResultSet rs) {
+
+		TransMonitor monitor = null;
+		try {
+			if (rs.next()) {
+				int i = rs.getInt("id");
+				String n = rs.getString("nombre");
+				int s = rs.getInt("stock");
+
+				monitor = new TransMonitor(i, n, s);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return monitor;
+	}
+
 	public ArrayList<TransMonitor> listarMonitor() {
-		// TODO Auto-generated method stub
-		return null;
+		ArrayList<TransMonitor> monitores = new ArrayList<>();
+		String query = "SELECT * FROM monitor";
+		try (PreparedStatement st = connection.prepareStatement(query)) {
+			ResultSet rs = st.executeQuery();
+			while (rs.next()) {
+				int i = rs.getInt("id");
+				String n = rs.getString("nombre");
+				int s = rs.getInt("salario");
+				TransMonitor monitor = new TransMonitor(i, n, s);
+				monitores.add(monitor);
+			}
+		} catch (SQLException e) {
+			System.err.print(e.getMessage());
+			e.printStackTrace();
+		}
+		return monitores;
+
 	}
 
 }
